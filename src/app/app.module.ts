4ae34ee9d4a879f0540f1fb0ce6680f1316aaa68
SvenLauterbach <svenlauterbach@googlemail.com>
2019-08-20 21:47:23 +0200

    Added profile service and protectedResourceMap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { MsalModule, MsalGuard, MsalInterceptor } from '@azure/msal-angular';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
+import { HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+
+import { ProfileService } from './profile/profile.service';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent},
@@ -17,24 +20,32 @@ const appRoutes: Routes = [
     pathMatch: 'full'
   }  
 ];
+export const protectedResourceMap:[string, string[]][]=[ ['https://buildtodoservice.azurewebsites.net/api/todolist',['api://a88bb933-319c-41b5-9f04-eff36d985612/access_as_user']] , ['https://graph.microsoft.com/v1.0/me', ['user.read']] ];
+
 
 
 @NgModule({
   imports:      [ 
     BrowserModule, 
     FormsModule,
+    HttpClientModule,
     RouterModule.forRoot(appRoutes,{useHash:true}),
      MsalModule.forRoot({
                     clientID: "5b0169f7-4491-4218-a671-5610bbcaabc1",
                     authority: "https://login.microsoftonline.com/common/",
                     validateAuthority: true,
-                    redirectUri: "http://localhost:4200/",
+                    redirectUri: "https://angular-msal-demo.stackblitz.io/",
                     cacheLocation : "localStorage",
-                    postLogoutRedirectUri: "http://localhost:4200/",
+                    postLogoutRedirectUri: "https://angular-msal-demo.stackblitz.io/",
                     navigateToLoginRequestUrl: true,
                     popUp: false,
+                    consentScopes: [ "user.read", "api://a88bb933-319c-41b5-9f04-eff36d985612/access_as_user"],
                     unprotectedResources: ["https://www.microsoft.com/en-us/"],
+                    protectedResourceMap: protectedResourceMap
                 })
+  ],
+  providers: [ProfileService,
+     {provide: HTTP_INTERCEPTORS, useClass: MsalInterceptor, multi: true}
   ],
   declarations: [ AppComponent, HelloComponent, HomeComponent, ProfileComponent ],
   bootstrap:    [ AppComponent ]
